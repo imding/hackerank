ffb3f19b4a385c6534839296141e6deb1e6af92d
Ding <minddevoursall@gmail.com>
2025-06-06 15:16:02 +0800

    Game of Thrones - I

---
+++ src/algorithm/game_of_thrones_i.rs
@@ -0,0 +1,63 @@
+fn game_of_thrones(s: &str) -> String {
+    let mut chars = s.chars().collect::<Vec<_>>();
+
+    chars.sort();
+
+    let (chunks, _) = chars
+        .into_iter()
+        .fold((Vec::with_capacity(26), '0'), |mut acc, char| {
+            if char != acc.1 {
+                let chunk = vec![char];
+
+                acc.0.push(chunk);
+                acc.1 = char;
+            } else {
+                match acc.0.last_mut() {
+                    Some(chunk) => chunk.push(char),
+                    _ => panic!("Expect previous chunk to be valid."),
+                };
+            }
+
+            acc
+        });
+    let mut odds = 0;
+
+    for chunk in chunks {
+        if odds > 1 {
+            break;
+        }
+
+        if chunk.len() % 2 > 0 {
+            odds += 1;
+        }
+    }
+
+    match odds {
+        0 | 1 => "YES",
+        _ => "NO",
+    }
+    .to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_game_of_thrones_01() {
+        let result = game_of_thrones("aaabbbb");
+        assert_eq!(result, "YES")
+    }
+
+    #[test]
+    fn test_game_of_thrones_02() {
+        let result = game_of_thrones("cdefghmnopqrstuvw");
+        assert_eq!(result, "NO")
+    }
+
+    #[test]
+    fn test_game_of_thrones_03() {
+        let result = game_of_thrones("cdcdcdcdeeeef");
+        assert_eq!(result, "YES")
+    }
+}
--- src/algorithm/mod.rs
+++ src/algorithm/mod.rs
@@ -1,5 +1,6 @@
 pub mod anagram;
 pub mod dna_health;
+pub mod game_of_thrones_i;
 pub mod making_anagram;
 pub mod palindrome_index;
 pub mod queens_attack_ii;
