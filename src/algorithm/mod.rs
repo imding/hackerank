219a2c2aa511336542f15fcb2db74ab65a3aa228
Ding <minddevoursall@gmail.com>
2025-06-06 17:41:08 +0800

    Sherlock and the Valid String

--- src/algorithm/mod.rs
+++ src/algorithm/mod.rs
@@ -4,5 +4,6 @@ pub mod game_of_thrones_i;
 pub mod making_anagram;
 pub mod palindrome_index;
 pub mod queens_attack_ii;
+pub mod sherlock_and_the_valid_string;
 pub mod string_construction;
 pub mod two_strings;
---
+++ src/algorithm/sherlock_and_the_valid_string.rs
@@ -0,0 +1,94 @@
+fn valid_string(s: &str) -> String {
+    let mut chars = s.chars().collect::<Vec<_>>();
+
+    chars.sort();
+
+    let (chunks, _) = chars.into_iter().fold((Vec::new(), '0'), |mut acc, char| {
+        if char != acc.1 {
+            let chunk = vec![char];
+
+            acc.0.push(chunk);
+        } else {
+            let chunk = match acc.0.last_mut() {
+                Some(chunk) => chunk,
+                _ => panic!("Expect last chunk to be valid."),
+            };
+
+            chunk.push(char);
+        }
+
+        acc.1 = char;
+
+        acc
+    });
+
+    println!("---");
+    println!("chunks: {:?}", chunks);
+
+    let lengths =
+        chunks
+            .iter()
+            .map(|chunk| chunk.len())
+            .fold(Vec::<Vec<usize>>::new(), |mut acc, length| {
+                match acc.iter().position(|chunk| chunk[0] == length) {
+                    Some(index) => {
+                        acc[index].push(length);
+                    }
+                    _ => acc.push(vec![length]),
+                };
+
+                acc
+            });
+
+    println!("lengths: {:?}", lengths);
+
+    match lengths.len() {
+        0 | 1 => "YES",
+        2 => match lengths.iter().any(|chunk| {
+            chunk.len() == 1
+                && (chunk[0] == 1 || (lengths[0][0] as i32 - lengths[1][0] as i32).abs() == 1)
+        }) {
+            true => "YES",
+            _ => "NO",
+        },
+        _ => "NO",
+    }
+    .to_string()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_valid_string_01() {
+        let result = valid_string("aabbcd");
+        assert_eq!(result, "NO");
+    }
+
+    #[test]
+    fn test_valid_string_02() {
+        let result = valid_string("aabbccddeefghi");
+        assert_eq!(result, "NO");
+    }
+
+    #[test]
+    fn test_valid_string_03() {
+        let result = valid_string("abcdefghhgfedecba");
+        assert_eq!(result, "YES");
+    }
+
+    #[test]
+    fn test_valid_string_04() {
+        let result = valid_string("aaaabbcc");
+        assert_eq!(result, "NO");
+    }
+
+    #[test]
+    fn test_valid_string_05() {
+        let result = valid_string(
+            "ibfdgaeadiaefgbhbdghhhbgdfgeiccbiehhfcggchgghadhdhagfbahhddgghbdehidbibaeaagaeeigffcebfbaieggabcfbiiedcabfihchdfabifahcbhagccbdfifhghcadfiadeeaheeddddiecaicbgigccageicehfdhdgafaddhffadigfhhcaedcedecafeacbdacgfgfeeibgaiffdehigebhhehiaahfidibccdcdagifgaihacihadecgifihbebffebdfbchbgigeccahgihbcbcaggebaaafgfedbfgagfediddghdgbgehhhifhgcedechahidcbchebheihaadbbbiaiccededchdagfhccfdefigfibifabeiaccghcegfbcghaefifbachebaacbhbfgfddeceababbacgffbagidebeadfihaefefegbghgddbbgddeehgfbhafbccidebgehifafgbghafacgfdccgifdcbbbidfifhdaibgigebigaedeaaiadegfefbhacgddhchgcbgcaeaieiegiffchbgbebgbehbbfcebciiagacaiechdigbgbghefcahgbhfibhedaeeiffebdiabcifgccdefabccdghehfibfiifdaicfedagahhdcbhbicdgibgcedieihcichadgchgbdcdagaihebbabhibcihicadgadfcihdheefbhffiageddhgahaidfdhhdbgciiaciegchiiebfbcbhaeagccfhbfhaddagnfieihghfbaggiffbbfbecgaiiidccdceadbbdfgigibgcgchafccdchgifdeieicbaididhfcfdedbhaadedfageigfdehgcdaecaebebebfcieaecfagfdieaefdiedbcadchabhebgehiidfcgahcdhcdhgchhiiheffiifeegcfdgbdeffhgeghdfhbfbifgidcafbfcd",
+        );
+        assert_eq!(result, "YES");
+    }
+}
